fd45c7aa953eff677783cd88da6126479147b18d
Brad Schrag <brad.schrag@gmail.com>
2018-11-26 18:31:29 +0000

    automatically backed up by learn

--- lib/ai/ai.rb
+++ lib/ai/ai.rb
@@ -6,14 +6,5 @@ class AI < Player
 
   def initialize(token)
     super
-    puts "What skill level for player #{@token}?"
-    puts "1) easy"
-    puts "2) unbeatable"
-    user_input =nil
-    if user_input != "1" || user_input != "2"
-      user_input = gets.strip
-    end
-    @skill = user_input.to_i
-    puts "skill set to #{@skill}"
   end
 end
--- lib/ai/scenario.rb
+++ lib/ai/scenario.rb
@@ -6,4 +6,3 @@ class Scenario
   end
 
 end
-    
