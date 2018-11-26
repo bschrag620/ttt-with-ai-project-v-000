b749ceec2f3f518e5e24e430edda5a04ab6af743
Brad Schrag <brad.schrag@gmail.com>
2018-11-26 18:16:56 +0000

    automatically backed up by learn

--- lib/ttt.rb
+++ lib/ttt.rb
@@ -6,17 +6,23 @@ require_relative "players/computer"
 puts "Welcome to Tic Tac Toe"
 user_input = nil
 while user_input != "exit"
+  play = false
   puts "How many players (type exit to quit)? "
   user_input = gets.strip
   case user_input.to_i
   when 0
     game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"))
+    play = true
   when 1
     game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
+    play = true
   when 2
     game = Game.new(Players::Human.new("X"), Players::Human.new("O"))
+    play =true
   else
     puts "Invalid input.Please try again."
   end
-  game.play
+  if play
+    game.play
+  end
 end
