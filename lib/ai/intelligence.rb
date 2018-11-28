def213b7a14a205edccab3ef3fe0dac484162c25
Brad Schrag <brad.schrag@gmail.com>
2018-11-28 14:02:13 +0000

    Automatically backed up by Learn

--- lib/ai/intelligence.rb
+++ lib/ai/intelligence.rb
@@ -1,4 +1,7 @@
-module Minimax
+require_relative "scenario.rb"
+require "./lib/players/computer"
+
+module Easymax
   module ClassMethods
 
   end
@@ -11,8 +14,88 @@ module Minimax
           valid_moves << i.to_s
         end
       end
+      puts "this was called from easymax"
       valid_moves.sample
     end
   end
 
+end
+
+module Minimax
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+    def valid_moves(board)
+      valid_moves = []
+      (1..9).each do |i|
+        if board.valid_move?(i)
+          valid_moves << i.to_s
+        end
+      end
+      valid_moves
+    end
+
+    def create_scenario(valid_moves)
+      valid_moves.each do |position|
+        self.class.scenarios << Scenario.new(position)
+      end
+    end
+
+    def test_position(position, board)
+      old_positions = board.cells
+      board.cells[position.to_i] = self.token
+      if self.game.won?
+        score = 10
+      elsif self.game.draw?
+        score = 0
+      else
+        if self.token == "X"
+          new_token = "O"
+        else
+          new_token = "X"
+        end
+        score = 0
+        new_player = Players::Computer.new(new_token)
+        new_valid_moves = valid_moves(board)
+        new_valid_moves.each do |new_move|
+          score -= (new_player.test_position(new_move, board))
+        end
+      end
+      board.cells = old_positions
+      score
+    end
+
+    def return_best_choice(array)
+      initial_score = 0
+      choices = []
+      array.each do |scenario|
+        if scenario.score > initial_score
+          initial_score = scenario.score
+          choices = []
+          choices << scenario
+        elsif scenario.score == initial_score
+          choices << scenario
+        end
+      end
+      choices
+    end
+
+    def move(board)
+      valid_moves = valid_moves(board)
+      create_scenario(valid_moves)
+      original_token = self.token
+      original_board = board
+      self.class.scenarios.each do |scenario|
+        self.token = original_token
+        self.game.board = board
+        scenario.score += test_position(scenario.position, board, self.token)
+      end
+      self.token = original_token
+      self.game.board = board
+      return_best_choice(self.class.scenarios).sample
+    end
+  end
 end
