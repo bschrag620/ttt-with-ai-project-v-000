9101cfe0503e12f06a9a1a17f2136bec48fe3b4c
Brad Schrag <brad.schrag@gmail.com>
2018-11-29 14:43:10 +0000

    Automatically backed up by Learn

--- lib/ai/ai.rb
+++ lib/ai/ai.rb
@@ -1,8 +1,13 @@
-class AI
-  attr_reader :token
-  attr_accessor :game
+require "./lib/player"
 
-  def initialize(token)
-    @token = token
+class AI < Player
+  attr_accessor :token
+  attr_accessor :skill, :game
+
+  def initialize(token, game)
+    super
+    puts "game initialzied to: #{game}"
+    # would like to add the ability to load different game logics depending on user choice
+
   end
 end
--- lib/ai/intelligence.rb
+++ lib/ai/intelligence.rb
@@ -1,4 +1,7 @@
-module Minimax
+require_relative "scenario.rb"
+require "./lib/players/computer"
+
+module Easymax
   module ClassMethods
 
   end
@@ -11,8 +14,30 @@ module Minimax
           valid_moves << i.to_s
         end
       end
+      puts "this was called from easymax"
       valid_moves.sample
     end
   end
 
+end
+
+module Minimax
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+    def valid_moves(board)
+      valid_moves = []
+      (1..9).each do |i|
+        if board.valid_move?(i)
+          valid_moves << i.to_s
+        end
+      end
+    end
+    
+    def move(board)
+      
+
+  end
 end
